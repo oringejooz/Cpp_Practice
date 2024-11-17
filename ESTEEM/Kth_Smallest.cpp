fcd16a1fb9b6b216ad51d24e3ea1c3ee0a8eda02
Swarnima <124855325+oringejooz@users.noreply.github.com>
2024-11-17 22:16:57 +0530

    new file:   ESTEEM/Kth_Smallest.cpp

---
+++ ESTEEM/Kth_Smallest.cpp
@@ -0,0 +1,17 @@
+// class Solution {
+//   public:
+//     // arr : given array
+//     // k : find kth smallest element and return using this function
+//     int kthSmallest(vector<int> &arr, int k) {
+//         priority_queue<int> pq;
+//         for(int i=0;i<k;i++){
+//             pq.push(arr[i]);
+//         }
+//         for(int i=k;i<arr.size();i++){
+//             if(pq.top()>arr[i]){
+//                 pq.pop();
+//                 pq.push(arr[i]);
+//             }
+//         }
+//         return pq.top();
+// };
